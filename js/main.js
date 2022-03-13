70c042fb3403cac3975a857a4495699d0501f86e
alexisMJI <alexismarin11@hotmail.es>
2022-03-13 16:03:41 -0300

    m1

--- js/main.js
+++ js/main.js
@@ -5,4 +5,31 @@ $(document).ready(function(){
       slideWidth: 1200
     });
 
+    //post
+    var posts = [
+      {
+       title: "Titulo del articulo 1",
+       date: "Publicado el dia: " + moment().date() + " de " + moment().format("MMMM") +" del "+ moment().format("YYYY"),
+       content: "Contenido Lorem impsu Lorem ipsum dolor sit amet, consectetur adipiscing elit. In orci lacus, pharetra non vehicula blandit, auctor ac odio. Suspendisse rhoncus nisl ipsum, a vehicula sem venenatis vel. Pellentesque mollis ligula quis erat egestas dignissim. Nulla a est dictum, euismod tellus non, fringilla erat. "
+      },
+      {
+        title: "Titulo del articulo 2",
+        date: "Publicado el dia: " + moment().date() + " de " + moment().format("MMMM") +" del "+ moment().format("YYYY"),
+        content: "Contenido Lorem impsu Lorem ipsum dolor sit amet, consectetur adipiscing elit. In orci lacus, pharetra non vehicula blandit, auctor ac odio. Suspendisse rhoncus nisl ipsum, a vehicula sem venenatis vel. Pellentesque mollis ligula quis erat egestas dignissim. Nulla a est dictum, euismod tellus non, fringilla erat. "
+       },
+       {
+        title: "Titulo del articulo 3",
+        date: "Publicado el dia: " + moment().date() + " de " + moment().format("MMMM") +" del "+ moment().format("YYYY"),
+        content: "Contenido Lorem impsu Lorem ipsum dolor sit amet, consectetur adipiscing elit. In orci lacus, pharetra non vehicula blandit, auctor ac odio. Suspendisse rhoncus nisl ipsum, a vehicula sem venenatis vel. Pellentesque mollis ligula quis erat egestas dignissim. Nulla a est dictum, euismod tellus non, fringilla erat. "
+       },
+       {
+        title: "Titulo del articulo 4",
+        date: "Publicado el dia: " + moment().date() + " de " + moment().format("MMMM") +" del "+ moment().format("YYYY"),
+        content: "Contenido Lorem impsu Lorem ipsum dolor sit amet, consectetur adipiscing elit. In orci lacus, pharetra non vehicula blandit, auctor ac odio. Suspendisse rhoncus nisl ipsum, a vehicula sem venenatis vel. Pellentesque mollis ligula quis erat egestas dignissim. Nulla a est dictum, euismod tellus non, fringilla erat. "
+       },
+    ]
+    console.log(posts);
+
+
+    
 });
